a46b53c5db38d9c762f78de3865bed572c011763
Nursat <zhaxybaevnursat@gmail.com>
2025-03-17 13:00:41 +0500

    Update actionbtn.dart

--- lib/actionbtn.dart
+++ lib/actionbtn.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 Widget actionButton(IconData icon, String actionTitle, Color iconColor) {
   return Expanded(
     child: TextButton.icon(
-      onPressed: () {},
+      onPressed: () {   },
       icon: Icon(icon, color: iconColor),
       label: Text(actionTitle, style: TextStyle(color: Colors.white)),
       style: TextButton.styleFrom(
